7687180f24579e4e859e33542c9952f5f7485491
Gavin Esplin <gdesplin@gmail.com>
2020-02-13 19:17:03 -0700

    Can add comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,40 @@
 class CommentsController < ApplicationController
-  def show
-  end
 
-  def new
-  end
+  before_action :user_signed_in?
 
   def create
+    @comment = Comment.create(safe_params.merge(user_id: current_user.id))
+    redirect_to @comment.post
   end
 
   def update
+    @comment = current_user.comments.find(params[:id])
+    authorize_user
+    @comment.update(safe_params)
   end
 
   def edit
+    @comment = current_user.comments.find(params[:id])
+    authorize_user
   end
 
   def destroy
+    @comment = current_user.comments.find(params[:id])
+    authorize_user
+    @comment.destroy
   end
+
+  private
+
+  def safe_params
+    params.require(:comment).permit(:body, :post_id)
+  end
+
+  def authorize_user
+    return true if @comment.user == current_user
+
+    flash.alert= "You're unauthorized to do that."
+    redirect_to :root
+  end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
 
-  before_actions :user_signed_in?, only: %i[create new update destroy]
+  before_action :user_signed_in?, except: :index
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def show
@@ -17,20 +17,20 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.create(safe_params)
     if @post.save
-      redirect_to :root
+      redirect_to @post
     else
       render :new
     end
   end
 
   def update
-    @post = current_user.find(params[:id])
+    @post = current_user.posts.find(params[:id])
     authorize_user
     @post.update(safe_params)
   end
 
   def edit
-    @post = current_user.find(params[:id])
+    @post = current_user.posts.find(params[:id])
     authorize_user
   end
 
@@ -48,6 +48,7 @@ class PostsController < ApplicationController
 
   def authorize_user
     return true if @post.user == current_user
+
     flash.alert= "You're unauthorized to do that."
     redirect_to :root
   end
